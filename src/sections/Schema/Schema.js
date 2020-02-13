2232bd83c255185e8b3ca6849a298a8f5d139432
Ke Li <keli95566@gmail.com>
2020-02-13 11:52:19 +0100

    upload component template

---
+++ src/sections/Schema/Schema.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Schema extends Component {
+  render() {
+    return <p> Schema</p>;
+  }
+}
+
+export default Schema;
--- src/sections/Schema/index.js
+++ src/sections/Schema/index.js
@@ -1,9 +1,1 @@
-import React, { Component } from "react";
-
-class Schema extends Component {
-  render() {
-    return <p> Schema</p>;
-  }
-}
-
-export default Schema;
+export { default } from "./Schema";
