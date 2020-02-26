2d9d1aceff4e85df811b8deffdb756696b872c90
Li <li_ke@dw-00039sl.intra.dlr.de>
2020-02-26 16:05:07 +0100

    clean up button groups

--- src/sections/AppToolbar/AppToolbar.js
+++ src/sections/AppToolbar/AppToolbar.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Button from "@material-ui/core/Button";
-import SvgIcon from "@material-ui/core/SvgIcon";
-import asd from "../../";
 import toolbarConfig from "../../configs/appToolbar";
 import "./AppToolbar.css";
 
@@ -11,7 +9,7 @@ class AppToolbar extends Component {
     const { appToolbarActions } = toolbarConfig;
     return (
       <div className="appToolbar-container ">
-        <div>Search bar will be here</div>
+        <div>A Search bar will be here</div>
 
         {appToolbarActions.map((item, key) => (
           <div className="appToolbar-buttonContainer">
