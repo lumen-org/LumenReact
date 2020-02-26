2a5180edbab81b059b2df3f1962260153d213324
Li <li_ke@dw-00039sl.intra.dlr.de>
2020-02-26 17:06:20 +0100

    add modal option

--- src/sections/MainWindows/MainWindows.js
+++ src/sections/MainWindows/MainWindows.js
@@ -2,14 +2,31 @@ import React, { Component } from "react";
 import PlotSettings from "../PlotSettings";
 import Playground from "../Playground";
 import AppToolbar from "../AppToolbar";
+import MainWindowsModal from "../../components/MainWindowsModal";
 import "./MainWindows.css";
 
 // TODO: Make it responsive according to tablet size
 class MainWindows extends Component {
+  state = {
+    openModal: false
+  };
+
+  handleModelClose = () => {
+    this.setState({
+      openModal: false
+    });
+  };
   render() {
+    const { openModal } = this.state;
+
     return (
       <div className="MainWindows-MainContainer">
         <AppToolbar />
+        <MainWindowsModal
+          open={openModal}
+          handleModelClose={this.handleModelClose}
+        />
+
         <div className="MainWindows-Container">
           <PlotSettings />
           <Playground />
