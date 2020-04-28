8333bf7d2228acdf4d8a86517842db0af2afaf00
Ke Li <keli95566@gmail.com>
2020-04-28 16:10:00 +0200

    WIP

--- src/sections/VisualizationCanvas/VisualizationCanvas.js
+++ src/sections/VisualizationCanvas/VisualizationCanvas.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Rnd } from "react-rnd";
-
+import CloseButton from "../../components/Button/CloseButton";
 import PropTypes from "prop-types";
 import "./VisualizationCanvas.css";
 
@@ -10,34 +10,53 @@ class VisualizationCanvas extends Component {
   };
 
   state = {
-    width: 100,
-    height: 100,
-    x: 100,
-    y: 100,
+    plotWindowsWidth: 500,
+    plotWindowsHeight: 500,
+    plotWindowsPosX: 100,
+    plotWindowsPosY: 100,
+  };
+
+  onDragStop = (event, dragIndex) => {
+    this.setState({
+      plotWindowsPosX: dragIndex.x,
+      plotWindowsPosY: dragIndex.y,
+    });
+  };
+
+  onResizeStop = (event, direction, ref, delta, position) => {
+    this.setState({
+      plotWindowsWidth: ref.style.width,
+      plotWindowsHeight: ref.style.height,
+      ...position,
+    });
+  };
+
+  handleClosePlot = () => {
+    // remove the plot information from the queue here.
   };
 
   render() {
     const { modelName } = this.props;
+    const {
+      plotWindowsHeight,
+      plotWindowsWidth,
+      plotWindowsPosX,
+      plotWindowsPosY,
+    } = this.state;
+
     return (
       <div className="VisualizationCanvas-container">
         <Rnd
-          size={{ width: this.state.width, height: this.state.height }}
-          position={{ x: this.state.x, y: this.state.y }}
-          onDragStop={(e, d) => {
-            this.setState({ x: d.x, y: d.y });
-          }}
-          onResizeStop={(e, direction, ref, delta, position) => {
-            this.setState({
-              width: ref.style.width,
-              height: ref.style.height,
-              ...position,
-            });
-          }}
-          style={{
-            boxShadow: "0 4px 16px rgba(0,0,0,0.2)",
-          }}
+          size={{ width: plotWindowsWidth, height: plotWindowsHeight }}
+          position={{ x: plotWindowsPosX, y: plotWindowsPosY }}
+          onDragStop={this.onDragStop}
+          onResizeStop={this.onResizeStop}
+          className="VisualizationCanvas-plot"
         >
-          {modelName}
+          <div className="VisualizationCanvas-plot-titlebar">
+            <CloseButton handleClose={this.handleClose} />
+            {modelName}
+          </div>
         </Rnd>
       </div>
     );
