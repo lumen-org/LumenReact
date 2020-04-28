22d7cd42d4128a8fac87fcd9711cb7ca2f5c7217
Ke Li <keli95566@gmail.com>
2020-04-28 15:11:19 +0200

    cleanup

--- src/states/app/reducer.js
+++ src/states/app/reducer.js
@@ -1,23 +1,15 @@
-import { SELECT_MODEL, SAVE_MODELS } from "../../constants/actionType";
+import { SELECT_MODEL } from "../../constants/actionType";
 
 export const defaultState = {
-  foundModels: [],
-  currentModel: "mcg_iris_map"
+  currentModel: "mcg_iris_map",
 };
 // TODO: add a check for default model.
 const app = (state = defaultState, action) => {
   switch (action.type) {
-    case SAVE_MODELS: {
-      return {
-        ...state,
-        foundModels: state.models.concat(action.payload)
-      };
-    }
-
     case SELECT_MODEL: {
       return {
         ...state,
-        currentModel: action.payload
+        currentModel: action.payload,
       };
     }
 
