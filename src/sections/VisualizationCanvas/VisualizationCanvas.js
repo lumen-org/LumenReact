4ec9968471846eee59bb18b924faec50da0cbf16
Ke Li <keli95566@gmail.com>
2020-04-28 14:59:54 +0200

    add react-rnd

--- src/sections/VisualizationCanvas/VisualizationCanvas.js
+++ src/sections/VisualizationCanvas/VisualizationCanvas.js
@@ -1,14 +1,46 @@
 import React, { Component } from "react";
+import { Rnd } from "react-rnd";
+
 import PropTypes from "prop-types";
 import "./VisualizationCanvas.css";
 
 class VisualizationCanvas extends Component {
   static propTypes = {
-    modelName: PropTypes.string
+    modelName: PropTypes.string,
   };
+
+  state = {
+    width: 100,
+    height: 100,
+    x: 100,
+    y: 100,
+  };
+
   render() {
     const { modelName } = this.props;
-    return <div className="VisualizationCanvas-container">{modelName}</div>;
+    return (
+      <div className="VisualizationCanvas-container">
+        <Rnd
+          size={{ width: this.state.width, height: this.state.height }}
+          position={{ x: this.state.x, y: this.state.y }}
+          onDragStop={(e, d) => {
+            this.setState({ x: d.x, y: d.y });
+          }}
+          onResizeStop={(e, direction, ref, delta, position) => {
+            this.setState({
+              width: ref.style.width,
+              height: ref.style.height,
+              ...position,
+            });
+          }}
+          style={{
+            boxShadow: "0 4px 16px rgba(0,0,0,0.2)",
+          }}
+        >
+          {modelName}
+        </Rnd>
+      </div>
+    );
   }
 }
 
