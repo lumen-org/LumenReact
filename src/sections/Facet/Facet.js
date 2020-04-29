dbdde4b9801300159a2ed4523c807314ebc6e921
Ke Li <keli95566@gmail.com>
2020-04-29 14:56:54 +0200

    update styles

--- src/sections/Facet/Facet.js
+++ src/sections/Facet/Facet.js
@@ -1,44 +1,52 @@
-import React from "react"
+import React from "react";
 import specificationFacetConfig from "../../configs/specificationFacetsConfig";
-import "./Facet.css"
-import PropTypes from "prop-types"
+import "./Facet.css";
 import PropTypes from "prop-types";
 
 class Facet extends React.Component {
-
   static propTypes = {
-    facets: PropTypes.object.isRequired
+    facets: PropTypes.object.isRequired,
   };
 
-  handleDataClick = key => {
-    this.props.onFacetDataUpdate(key);
+  handleDataClick = (key) => {
+    const { onFacetDataUpdate } = this.props;
+    onFacetDataUpdate(key);
   };
 
-  handleModelClick = key => {
-    this.props.onFacetModelUpdate(key);
+  handleModelClick = (key) => {
+    const { onFacetModelUpdate } = this.props;
+    onFacetModelUpdate(key);
   };
 
-  render(){
+  render() {
     const { facetsActions } = specificationFacetConfig;
     const { facets } = this.props;
-    const { openModal } = true;
     return (
       <div className="facet-bar ">
         {facetsActions.map((item, key) => (
           <div className="facet-label">
-            <div>
-              <img src={item.icon} alt="" />
-              {item.name}<input type="checkbox" onClick={() => this.handleModelClick(key)}
-                                checked={facets[key].model}
-            /><input type="checkbox" onClick={() => this.handleDataClick(key)}
-                     checked={facets[key].data}
-            />
+            <div className="specifications-facets-item">
+              <img
+                src={item.icon}
+                alt=""
+                className="specifications-facets-img"
+              />
+              {item.name}
+              <input
+                type="checkbox"
+                onClick={() => this.handleModelClick(key)}
+                checked={facets[key].model}
+              />
+              <input
+                type="checkbox"
+                onClick={() => this.handleDataClick(key)}
+                checked={facets[key].data}
+              />
             </div>
           </div>
         ))}
       </div>
-    )
+    );
   }
-
 }
 export default Facet;
