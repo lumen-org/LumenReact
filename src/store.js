a31a7a7cae1bf38110f73157ae0325906148a15f
laureleya <sarah.bachinger@uni-jena.de>
2020-06-03 10:58:54 +0200

    changed composedEnhancers

--- src/store.js
+++ src/store.js
@@ -5,7 +5,8 @@ import reducer from "./reducer";
 // for more information read here:
 // https://redux.js.org/recipes/configuring-your-store
 // if it gets larger please consider creating a configureStore() function
-const composedEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ serialize: true }) || compose;
+const composedEnhancers = typeof window === "object" &&
+window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ serialize: true }) : compose;
 const store = createStore(reducer,
   undefined,
   composedEnhancers(applyMiddleware(thunk))
