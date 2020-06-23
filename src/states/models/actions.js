67c2b33e8f72102ea3a33d8407b755615bb30a4f
Schmalwasser, Laines <laines.schmalwasser@dlr.de>
2020-06-23 17:26:28 +0200

    uuidv4 for model

--- src/states/models/actions.js
+++ src/states/models/actions.js
@@ -1,8 +1,12 @@
 import { CREATE_NEW_MODEL } from "./constants";
+import { v4 as uuidv4 } from 'uuid';
 
-export const createNewModel = (payload) => {
+export const createNewModel = (model, id=uuidv4()) => {
   return {
     type: CREATE_NEW_MODEL,
-    payload
+    payload: {
+      model,
+      id
+    }
   }
 };
