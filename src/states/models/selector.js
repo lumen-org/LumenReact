dea14ccc92701beebf8d2c97b90c8f90f1125f56
laureleya <sarah.bachinger@uni-jena.de>
2020-07-08 11:03:35 +0200

    make dimensions droppable again

--- src/states/models/selector.js
+++ src/states/models/selector.js
@@ -7,7 +7,7 @@ const getActiveId = (state) => selectActiveModelId(state);
 /// not sure if that is correct now...
 export const getModelNameById = (state, id) => {
   //return state.models.models.byId[id].modelName || "";
-  return state.models.models.byId[state.plots.plots.byId[id].visualizationId].modelName || "";
+  return state.models.models.byId[state.visualizations.visualizations.byId[state.plots.plots.byId[id].visualizationId].modelId].modelName || "";
 
 };
 
