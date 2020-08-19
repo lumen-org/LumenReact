5b369767ee9322b884c0d8aa36efaa3d4a474cc8
Schmalwasser, Laines <laines.schmalwasser@dlr.de>
2020-08-19 11:05:52 +0200

    extracted ids

--- src/components/ListModal/ListModalContainer.js
+++ src/components/ListModal/ListModalContainer.js
@@ -1,15 +1,13 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { updateActiveModel } from "../../states/app/actions";
 import { createNewSpecification } from "../../states/specifications/actions";
 import { createNewPlot } from "../../states/plots/actions";
 import PropTypes from "prop-types";
 import ListModal from "./ListModal";
 import fetchData, { fetchModelData } from "../../utils/fetch";
 import { BASE_URL, FETCH_ALL_MODEL_NAME } from "../../constants/query";
-import { changeActiveVisualization, createNewVisualization, fillVisualization } from "../../states/visualizations/actions";
+import { changeActiveVisualization, createNewVisualization } from "../../states/visualizations/actions";
 import { createNewModel } from "../../states/models/actions";
-import visualizations from "../../states/visualizations/reducer";
 import { generateId } from "../../utils/idGenerator";
 
 class ListModalContainer extends React.Component {
@@ -22,35 +20,6 @@ class ListModalContainer extends React.Component {
     models: [],
     currentModel: null
   };
-
-  getModelData = async (item) => {
-    const {
-      changeActiveVisualization,
-      handleModalClose,
-      createPlot,
-      addSpecifications,
-      createNewVisualization,
-      createNewModel,
-      fillVisualization,
-      lastCreatedVisualizationId,
-      specificationId,
-      modelId
-    } = this.props;
-    createNewVisualization(item).then(() => {
-      addSpecifications().then(() => {
-        // move into schema redux store to avoid this nested promises
-        fetchModelData(item).then((response) => {
-          createNewModel(item.modelName, response["Fields"]);
-        }
-        ).then(() => {
-          createPlot(item, this.props.lastCreatedVisualizationId, this.props.specificationId);
-          fillVisualization(this.props.lastCreatedVisualizationId, this.props.modelId, this.props.specificationId, this.props.plotId);
-          changeActiveVisualization(this.props.lastCreatedVisualizationId);
-          handleModalClose();
-        });
-      });
-    });
-  }
 
   handleItemSelection = async (item) => {
     const {
@@ -67,9 +36,10 @@ class ListModalContainer extends React.Component {
     const specId = generateId();
     const plotId = generateId();
     const visId = generateId();
+
     createNewVisualization(visId, id, specId, plotId)
     createNewSpecification(specId)
-    createNewModel(item, Fields)
+    createNewModel(item, Fields, id)
     createPlot(item, visId, specId)
     changeActiveVisualization(visId)
     handleModalClose();
@@ -109,17 +79,13 @@ const mapDispatchToProps = (dispatch) => {
   return {
     createNewVisualization: (id, modelId, specificationId, plotId) =>
       dispatch(createNewVisualization(id, modelId, specificationId, plotId)),
-    updateActiveModel: (model) => dispatch(updateActiveModel(model)),
     changeActiveVisualization: (id) => dispatch(changeActiveVisualization(id)),
     createPlot: (activeModel, visualizationId, specificationId) =>
       dispatch(createNewPlot(activeModel, visualizationId, specificationId)),
-    // resetSpecifications: () => dispatch(resetSpecifications()),
     createNewSpecification: (specId) => {
       return dispatch(createNewSpecification(specId));
     },
-    createNewModel: (modelName, model) => dispatch(createNewModel(modelName, model)),
-    fillVisualization: (visualizationId, modelId, specificationId, plotId) =>
-      dispatch(fillVisualization(visualizationId, modelId, specificationId, plotId))
+    createNewModel: (modelName, model, modelId) => dispatch(createNewModel(modelName, model, modelId))
   };
 };
 
