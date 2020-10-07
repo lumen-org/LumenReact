6d04d048c9831f8601ea2f3b1b0e8d8df225f562
Schmalwasser, Laines <laines.schmalwasser@dlr.de>
2020-10-07 13:45:28 +0200

    removed unused tabs

--- src/configs/appToolbar.js
+++ src/configs/appToolbar.js
@@ -6,49 +6,7 @@ const toolbarConfig = {
       color: "primary",
       variant: "contained"
     },
-    {
-      name: "query",
-      variant: "outlined",
-      color: "light",
-      icon: require("../assets/icons/adjust-24px.svg"),
-    },
-    {
-      name: "clone",
-      color: "light",
-      variant: "outlined",
-      icon: require("../assets/icons/file_copy-24px.svg"),
-    },
-    {
-      name: "undo",
-      color: "light",
-      variant: "outlined",
-      icon: require("../assets/icons/undo-24px.svg"),
-    },
-    {
-      name: "redo",
-      color: "light",
-      variant: "outlined",
-      icon: require("../assets/icons/redo-24px.svg"),
-    },
-    {
-      name: "clear",
-      color: "light",
-      variant: "outlined",
-      icon: require("../assets/icons/clear_all-24px.svg"),
-    },
-    {
-      name: "config",
-      color: "light",
-      variant: "outlined",
-      icon: require("../assets/icons/settings-24px.svg"),
-    },
-    {
-      name: "graph",
-      color: "light",
-      variant: "outlined",
-      icon: require("../assets/icons/linear_scale-24px.svg"),
-    },
-  ],
+    ],
 };
 
 export default toolbarConfig;
