f7903b81a88b66063cd8e32e71e74c0271d19862
Ke Li <keli95566@gmail.com>
2020-10-10 21:42:34 +0200

    quick fix bug of react plotly

--- src/components/StandardPlot/StandardPlot.js
+++ src/components/StandardPlot/StandardPlot.js
@@ -4,11 +4,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import "./StandardPlot.css";
 import { defaultPlot } from "./defaultPlot";
-// We need to import Plotly in this strange way due to heap memory
-// See issue: https://github.com/plotly/react-plotly.js/issues/135
-import createPlotlyComponent from "react-plotly.js/factory";
-const Plotly = window.Plotly;
-const Plot = createPlotlyComponent(Plotly);
+import Plot from "react-plotly.js";
 
 class StandardPlot extends Component {
   static propTypes = {
