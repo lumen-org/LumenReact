672a4ce9dc0b0823654b3fac231819730a2d46c0
Ke Li <keli95566@gmail.com>
2020-11-10 15:13:20 +0100

    setup skeleton

--- src/utils/pqlPredictQueries.js
+++ src/utils/pqlPredictQueries.js
@@ -2,3 +2,48 @@
  * This utility module contains functions that process PQL queries for data processing.
  * See https://github.com/lumen-org/LumenReact/wiki/PQL-API for understanding the PQL grammar.
  */
+
+/**
+ *
+ * @param {*} predictClause required
+ * @param {*} fromCaluse required
+ * @param {*} whereCaluse optional
+ * @param {*} splitbyClause optional
+ * predict-expr = predict-clause, from-clause , [where-clause], [split-by-clause];
+ */
+function createPredictionExpression(
+  predictClause,
+  fromCaluse,
+  whereCaluse = null,
+  splitbyClause = null
+) {}
+
+/**
+ * predict-clause = "PREDICT" , (dimension | aggr-dimension) , {dimension | aggr-dimension};
+ * @param {*} dimension
+ * @param {*} aggregation  "probability", "maximum", "minimum", "average" etc
+ * @param {*} _class "denisty"
+ * @param {*} yields yields of the prediction
+ * @param {*} name name of the dimension
+ */
+function createPredictionClause(
+  dimension,
+  aggregation = null,
+  _class = null,
+  yields = null,
+  name = null
+) {}
+
+/**
+ *
+ * @param {*} name name of the dimension
+ * @param {*} split
+ * @param {*} args
+ * @param {*} _class
+ */
+function createSplitByClause(
+  name = "dimensionName",
+  split = "equiinterval",
+  args = 25,
+  _class = "Split"
+) {}
