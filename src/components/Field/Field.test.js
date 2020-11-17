a531d4efe37e004caa1b56427a495b61b0f969a0
laureleya <sarah.bachinger@uni-jena.de>
2020-11-17 16:26:30 +0100

    added tests to fields.test.js

--- src/components/Field/Field.test.js
+++ src/components/Field/Field.test.js
@@ -4,11 +4,9 @@ import { act } from "react-dom/test-utils";
 import TitelH2 from "../Titles/TitleH2";
 import FieldList from "../FieldList/FieldList";
 //import { render, fireEvent, screen } from "@testing-library/react";
-import { shallow } from 'enzyme';
+import { shallow } from "enzyme";
 import Field from "./Field";
 import pretty from "pretty";
-
-
 
 let container = null;
 beforeEach(() => {
@@ -42,14 +40,29 @@ it("missing title 2", () => {
 });
 
 */
-it("render normally", () => {    
-  let title = "TestTitle";
-  let data = ["Test", "Test", "Test"];
-  const wrapper = shallow((
-    <Field title={title} data={data} />
-  ));
-  expect(wrapper.contains(<TitelH2 value={title}/>,
-    <FieldList dataList={data}/>)).toEqual(true);
-
-      
+describe("Field.js Test", () => {
+  it("render normally", () => {
+    let title = "TestTitle";
+    let data = ["Test", "Test", "Test"];
+    jest.spyOn(console, "error");
+    const wrapper = shallow(<Field title={title} data={data} />);
+    expect(
+      wrapper.contains(<TitelH2 value={title} />, <FieldList dataList={data} />)
+    ).toEqual(true);
+    expect(console.error).not.toBeCalled();
   });
+
+  it("missing value title", () => {
+    let title = "TestTitle";
+    jest.spyOn(console, "error");
+    const wrapper = shallow(<Field title={title} />);
+    expect(console.error).toBeCalled();
+  });
+
+  it("missing value data", () => {
+    let data = ["TestTitle"];
+    jest.spyOn(console, "error");
+    const wrapper = shallow(<Field data={data} />);
+    expect(console.error).toBeCalled();
+  });
+});
--- src/setupTests.js
+++ src/setupTests.js
@@ -6,3 +6,19 @@ import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 
 window.URL.createObjectURL = function() {};
+
+//from https://github.com/facebook/prop-types/issues/28#issuecomment-521700117
+/*jest.mock('prop-types/checkPropTypes', () => {
+    return (...args) => {
+      const checkPropTypes = jest.requireActual('prop-types/checkPropTypes');
+      const originalConsoleError = console.error;
+      console.error = function(...args) {
+        process.nextTick(() => {
+          throw new Error(...args);
+        });
+      };
+      const result = checkPropTypes(...args);
+      console.error = originalConsoleError;
+      return result;
+    };
+  });*/
