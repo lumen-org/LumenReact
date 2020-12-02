c843033264f8328a6749b3310dec0deab7f5c4d5
Laines Schmalwasser <laines@web.de>
2020-12-02 11:05:17 +0100

    default tests for standardspecifications

--- src/states/standardspecifications/reducer.test.js
+++ src/states/standardspecifications/reducer.test.js
@@ -1,8 +1,82 @@
 import * as types from "./constants";
 import reducer from "./reducer";
+import { defaultValues, defaultState, filledOneState, changedPredictionModelState } from "./testData";
 
 describe("standardspecification reducer", () => {
-    it("should create the intial state", () => { 
-        
+    it("should create the intial state", () => {
+        expect(reducer(undefined, {})).toEqual(
+            {
+                standardspecifications: {}
+            }
+        )
+    })
+
+    it("should create new standardspecification", () => {
+        const id = 10
+
+        expect(reducer(undefined, {
+            type: types.CREATE_NEW_STANDARD_SPECIFICATION,
+            payload: {
+                id,
+                defaultValues
+            }
+        })).toEqual(defaultState)
+    })
+
+    it("should remove a standardspecification", () => {
+        const payload = {
+            id: 10
+        }
+
+        expect(reducer(defaultState, {
+            type: types.REMOVE_STANDARD_SPECIFICATION,
+            payload
+        })).toEqual({ standardspecifications: {} })
+    })
+
+    it("should add new value to standardspecification", () => {
+        const payload = {
+            id: 10,
+            key: "X_Axis",
+            value: "time[s]"
+        }
+
+        expect(reducer(defaultState, {
+            type: types.ADD_TO_STANDARD_SPECIFICATION,
+            payload
+        })).toEqual(filledOneState)
+    })
+
+    it("should delete a value from standardspecification", () => {
+        const payload = {
+            id: 10,
+            key: "X_Axis",
+            value: "time[s]"
+        }
+
+        expect(reducer(filledOneState, {
+            type: types.DELETE_FROM_STANDARD_SPECIFICATION,
+            payload
+        })).toEqual(defaultState)
+    })
+
+    it("should activate prediction model facet from standardspecification", () => {
+        const payload = {
+            id: 10,
+            type: "model",
+            key: "Prediction"
+        }
+
+        expect(reducer(defaultState, {
+            type: types.UPDATE_FACET_STATE,
+            payload
+        })).toEqual(changedPredictionModelState)
+    })
+
+    it("should reset the standardspecification", () => {
+
+        expect(reducer(defaultState, {
+            type: types.RESET_STANDARD_SPECIFICATIONS
+        })).toEqual({standardspecifications: {}})
     })
 })
--- src/states/standardspecifications/testData.js
+++ src/states/standardspecifications/testData.js
@@ -87,3 +87,105 @@ export const filledTwoValues = {
         },
     },
 };
+
+export const defaultState = {
+    standardspecifications: {
+        '10': {
+            specification: {
+                X_Axis: new Set([]),
+                Y_Axis: new Set([]),
+                Filter: new Set([]),
+                Detail: new Set([]),
+                Color: new Set([]),
+                Shape: new Set([]),
+                Size: new Set([]),
+            },
+            facets: {
+                Prediction: {
+                    model: false,
+                    data: false,
+                },
+                "Data Points": {
+                    model: false,
+                    data: true,
+                },
+                Marginals: {
+                    model: false,
+                    data: true,
+                },
+                Density: {
+                    model: false,
+                    data: false,
+                },
+            },
+        }
+    }
+}
+
+export const filledOneState = {
+    standardspecifications: {
+        '10': {
+            specification: {
+                X_Axis: new Set(["time[s]"]),
+                Y_Axis: new Set([]),
+                Filter: new Set([]),
+                Detail: new Set([]),
+                Color: new Set([]),
+                Shape: new Set([]),
+                Size: new Set([]),
+            },
+            facets: {
+                Prediction: {
+                    model: false,
+                    data: false,
+                },
+                "Data Points": {
+                    model: false,
+                    data: true,
+                },
+                Marginals: {
+                    model: false,
+                    data: true,
+                },
+                Density: {
+                    model: false,
+                    data: false,
+                },
+            },
+        }
+    }
+}
+
+export const changedPredictionModelState = {
+    standardspecifications: {
+        '10': {
+            specification: {
+                X_Axis: new Set([]),
+                Y_Axis: new Set([]),
+                Filter: new Set([]),
+                Detail: new Set([]),
+                Color: new Set([]),
+                Shape: new Set([]),
+                Size: new Set([]),
+            },
+            facets: {
+                Prediction: {
+                    model: true,
+                    data: false,
+                },
+                "Data Points": {
+                    model: false,
+                    data: true,
+                },
+                Marginals: {
+                    model: false,
+                    data: true,
+                },
+                Density: {
+                    model: false,
+                    data: false,
+                },
+            },
+        }
+    }
+}
